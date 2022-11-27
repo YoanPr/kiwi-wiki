81ecaebd286dba2402bcf70bb9d32acd37655a9f
Yoan Prosdocimi <yoanprosdocimi@gmail.com>
2022-11-27 16:01:53 +0100

    Mise en place base de données

--- backend/src/knexfile.js
+++ backend/src/knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: 'src/dev.sqlite3'
     },
     useNullAsDefault: true,
 
---
+++ backend/src/migrations/20221127131539_init.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('page', table => {
+       table.string('url').primary()
+       table.text('texte')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+      .dropTable('page')
+};
