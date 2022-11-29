68af2c060fa414b9e0338d2ed672e271bc293810
Yoan Prosdocimi <yoanprosdocimi@gmail.com>
2022-11-29 22:15:19 +0100

    Ajout des méthodes pour récuperer les articles

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,12 +8,16 @@ const database = knex(dbConfig.development)
 
 const app = express(feathers())
 
+// On ajoute Cors pour permettre au front de faire des requetes
+const cors = require('cors')
+app.use(cors())
+
 // JSON body parser middleware
 app.use(express.json())
 // Set up REST transport
 app.configure(express.rest())
 
-// Feathers/Knex REST services
+// Feathers/Knex REST services, met en place le service pour la table articles
 app.use('/api/articles', knexService({ Model: database, name: 'articles' }))
 
 app.listen(3000, function () {
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,33 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Vue.js'
+    }
+  },
+  methods: {
+    async getToutLesArticles(){
+      let response = await fetch('http://localhost:3000/api/articles')
+      let articles = await response.json()
+      return articles
+    },
+    async getArticleFromUrl(url) {
+      let response = await fetch('http://localhost:3000/api/articles?url=' + url)
+      let article = await response.json()
+      return article[0]
+     },
+    async getArticlesFromTitre(titre) {
+        let response = await fetch('http://localhost:3000/api/articles?titre[$like]=%' + titre + '%')
+        let articles = await response.json()
+        return articles
+     }
+   }
+}
+</script>
+
 <template>
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+
 </template>
