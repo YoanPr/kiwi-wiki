e2009d077df5437f8a83e79734859460ce5a06ef
Yoan Prosdocimi <yoanprosdocimi@gmail.com>
2022-12-13 21:42:52 +0100

    Résolution d'un problème lors de l'accès d'un article déjà ajouté, il était considéré comme nouveau

--- frontend/src/components/Empty.vue
+++ frontend/src/components/Empty.vue
@@ -3,18 +3,13 @@
     <Creation v-else :paramUrl="url" />
 </template>
 <script setup>
-    import NavBar from './NavBar.vue'
     import Visualisation from './Visualisation.vue'
     import Creation from './Creation.vue'
     import useArticles from '../useArticles.js'
-    import { useRouter, useRoute } from 'vue-router';
 
     const getArticleFromURL = useArticles['methods']['getArticleFromURL']
-    const route = useRoute()
-    const url = (route.params.proprieteUrlArticle).join('/')
-    //const v = 3.14
-    const article = await getArticleFromURL(url)
-    console.log(article)
+
+    const article = await getArticleFromURL(window.location.pathname)
 
     function found(){
        return article !== null;
