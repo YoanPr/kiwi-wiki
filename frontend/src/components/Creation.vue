6759bad31f586d6c41ce4725808be3417036b43b
CAN <andriatoc@live.fr>
2022-12-14 23:32:00 +0100

    Texte des boutons et libellés sur le formulaire

--- frontend/src/components/Creation.vue
+++ frontend/src/components/Creation.vue
@@ -2,15 +2,15 @@
     <NavBar />
     <div class="bg-indigo-100">
         <div class="container grid place-items-center max-w-3xl mx-auto gap-y-1 bg-indigo-100">
-            <p class="text-center my-4">Oups ! Aucun article n'est disponible à cette URL, mais vous pouvez dès maintenant en ajouter un nouveau en utilisant le formulaire ci-dessous ! </p>
-            <h3 class="text-2xl mt-2">Création Article</h3>
+            <p v-if="isNew" class="text-center my-4">Oups ! Aucun article n'est disponible à cette URL, mais vous pouvez dès maintenant en ajouter un nouveau en utilisant le formulaire ci-dessous ! </p>
+            <h3 class="text-2xl mt-2">{{ isNew ? "Ajout" : "Modification "}} article</h3>
             <h4 class="mb-3"> Adresse de l'article : {{url}}</h4>
             <input class="mb-3" id="titreArticle" type="text" placeholder="Titre article" v-bind:value="title"><br>
             <Edition :paramContenuArticle=contenu />
             <input class="mb-3" id="textArticle" type="hidden" placeholder="Texte article"><br>
             <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
             id="boutonAjout" type="button" v-on:click="creationArticle(isNew)">
-                Ajouter
+                {{ isNew ? "Ajouter" : "Modifier "}}
             </button>
         </div>
     </div>
