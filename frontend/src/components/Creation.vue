f93698b00d58a4ed335db5bcb767ce344e425f9c
Mathilde <mathilde.nhieu@gmail.com>
2022-12-15 19:16:38 +0100

    Changement de place bouton changer de mode

--- frontend/src/components/Creation.vue
+++ frontend/src/components/Creation.vue
@@ -12,12 +12,11 @@
             <div class="p-5 mt-auto place-items-center ">
                 <button class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                     id="boutonAjout" type="button" v-on:click="creationArticle(isNew)">
-                    {{ isNew ? "Ajouter" : "Modifier "}}
+                    {{ isNew ? "Ajouter" : "Valider la modification"}}
                 </button>
             </div>
         </div>
     </div>
-    <Footer />
 </template>
   
 <script setup>    
