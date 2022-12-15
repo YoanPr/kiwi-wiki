8229d9c7d949f8916146e83eeab0d721214ca589
jonathanbonnefoy <jonathan.bonnefoy@outlook.com>
2022-12-16 00:50:34 +0100

    Ajout du lien de la vidéo YT de démo

--- frontend/src/components/Demonstration.vue
+++ frontend/src/components/Demonstration.vue
@@ -5,7 +5,7 @@
             <p class="text-center text-3xl my-6">Vidéo de démonstration!</p>
          
             <iframe width="800" height="500"
-                src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                src="https://youtu.be/Q2QYRn8oT2A">
             </iframe> 
             <div class="p-5 mt-auto place-items-center ">
                 <button class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
