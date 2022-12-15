be73565a899c597ad1277a145982574a8218e26d
Mathilde <mathilde.nhieu@gmail.com>
2022-12-15 21:46:16 +0100

    Correction en cours sur bouton "Changer de mode"

--- frontend/src/components/Routage_Article.vue
+++ frontend/src/components/Routage_Article.vue
@@ -1,19 +1,29 @@
 <template>
     <div class="bg-indigo-100">
-    <div class="bg-indigo-100" v-if="found()">
+    <div v-if="found()">
         
         <Visualisation  v-if="modeVisualisation" :paramArticle=article>
-        
+            <div class="container grid place-items-center max-w-3xl mx-auto gap-y-1 bg-indigo-100">
+                <div class="p-5 mt-auto place-items-center ">
+                <button type="button" v-on:click="changeMode" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Edition</button>
+            </div>
+            </div>
         </Visualisation>
-        <Creation v-else :paramArticle=article />
+        <Creation v-else-if="!modeVisualisation" :paramArticle=article>
+        <!-- <Creation v-else :paramArticle=article> -->
+            <div class="container grid place-items-center max-w-3xl mx-auto gap-y-1 bg-indigo-100">
+                <div class="p-5 mt-auto place-items-center ">
+                <button type="button" v-on:click="changeMode" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Visualisation</button>
+            </div>
+            </div>
+        </Creation>
+        <div class="container grid place-items-center max-w-3xl mx-auto gap-y-1 bg-indigo-100">
+            <div class="p-5 mt-auto place-items-center ">
+            <button type="button" v-on:click="changeMode" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Changer de mode </button>
+        </div>
+        </div>  
     </div>
     <Creation v-else :paramUrl="url" />
-    <div class="container grid place-items-center max-w-3xl mx-auto gap-y-1 bg-indigo-100">
-        <div class="p-5 mt-auto place-items-center ">
-        <button type="button" v-on:click="changeMode" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Changer de mode </button>
-    </div>
-    </div>
-    
     <Footer />
 </div>
 </template>
@@ -36,7 +46,7 @@
     let modeVisualisation = ref(true)
     function changeMode() {
         modeVisualisation.value = !modeVisualisation.value
-        console.log(modeVisualisation)
+        console.log(modeVisualisation.value)
     } 
 
     function found(){
