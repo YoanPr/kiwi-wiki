1f449b4f18d2180bd1ec4a9721d528d871664645
Mathilde <mathilde.nhieu@gmail.com>
2022-12-15 21:56:42 +0100

    Bouton switch Edition/Visualisation

--- frontend/src/components/Routage_Article.vue
+++ frontend/src/components/Routage_Article.vue
@@ -3,26 +3,26 @@
     <div v-if="found()">
         
         <Visualisation  v-if="modeVisualisation" :paramArticle=article>
+        </Visualisation>
+        <Creation v-else-if="!modeVisualisation" :paramArticle=article>
+        </Creation>
+
+        <div v-if="modeVisualisation">
             <div class="container grid place-items-center max-w-3xl mx-auto gap-y-1 bg-indigo-100">
                 <div class="p-5 mt-auto place-items-center ">
                 <button type="button" v-on:click="changeMode" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Edition</button>
             </div>
             </div>
-        </Visualisation>
-        <Creation v-else-if="!modeVisualisation" :paramArticle=article>
-        <!-- <Creation v-else :paramArticle=article> -->
+        </div>
+        <div v-else >
             <div class="container grid place-items-center max-w-3xl mx-auto gap-y-1 bg-indigo-100">
                 <div class="p-5 mt-auto place-items-center ">
                 <button type="button" v-on:click="changeMode" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Visualisation</button>
             </div>
             </div>
-        </Creation>
-        <div class="container grid place-items-center max-w-3xl mx-auto gap-y-1 bg-indigo-100">
-            <div class="p-5 mt-auto place-items-center ">
-            <button type="button" v-on:click="changeMode" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Changer de mode </button>
         </div>
-        </div>  
-    </div>
+        
+        </div>
     <Creation v-else :paramUrl="url" />
     <Footer />
 </div>
@@ -34,6 +34,7 @@
     import Footer from './Footer.vue'
     import {ref} from 'vue'
     import { useRouter, useRoute } from 'vue-router'
+import { getMemoedVNodeCall } from '@vue/compiler-core'
 
     const getArticleFromURL = useArticles['methods']['getArticleFromURL']
 
